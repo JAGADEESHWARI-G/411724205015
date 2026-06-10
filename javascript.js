17dd6d73225b6d7155ac70b07a69e88686866bbb
jagadeeshwari <jagadha2007@gmail.com>
2026-06-10 15:09:24 +0530

    JS

---
+++ javascript.js
@@ -0,0 +1,10 @@
+console.log("print statement")
+const a=10;
+const b=20;
+console.log(a+b)
+const str="jaga"
+console.log(str)
+const arr=[11,11.01,"sj",true]
+for(let i=0;i<arr.length;i++){
+console.log(arr[i])
+}
